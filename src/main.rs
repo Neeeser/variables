a0fdc7d121ef7ba6d8bc54a8284abf9a49cbbd12
Neeeser <andrew.neeser@me.com>
2023-06-13 14:16:44 -0400

    Arrays

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ fn main() {
     println!("The value of x is: {x}");
 
     operations();
+    booleans();
+    tuples();
+    array_out_of_bounds_test();
 }
 
 fn operations() {
@@ -25,4 +28,60 @@ fn operations() {
 
     let remainder = 43 % 5;
     println!("Remained is: {remainder}");
+}
+
+fn booleans() {
+    let t = true;
+
+    let f: bool = false;
+}
+
+fn characters() {
+    let c = 'z';
+    let z: char = 'Z';
+    let heart_eyed_cat: char = '😻';
+}
+
+fn tuples() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let five_hundred = tup.0;
+    println!("The first value is: {five_hundred}")
+}
+
+fn arrays() {
+    let a = [1, 2, 3, 4, 5];
+
+    // this is the same as doing let a = [3,3,3,3,3];
+    let a = [3; 5];
+
+    let first = a[0];
+    let second = a[1];
+}
+
+use std::io;
+
+fn array_out_of_bounds_test() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
